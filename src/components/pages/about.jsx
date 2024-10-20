1a7574481e1433c248d332fdfe72f73356d44b1a
sebastianbraeutigam <sebastian.lange4@icloud.com>
2024-10-20 10:22:02 +0200

    change about.jsx

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class About extends Component {
     state = {  } 
     render() { 
-        return  <React.Fragment>
+        return  <>
                     <h2>Über uns</h2>
                     <b>Herzlich Willkommen im virtuellen Familienzentrum Dessau!</b>
-                </React.Fragment>;
+                </>;
     }
 }
  
