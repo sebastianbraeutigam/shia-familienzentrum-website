4f9e1c68d83a932e7633f76ee998f008198c6911
sebastianbraeutigam <sebastian.lange4@icloud.com>
2024-11-09 17:00:54 +0100

    changes home.jsx

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -6,7 +6,7 @@ class Home extends Component {
         return  <>
                     <h1>SHIA Familienzentrum</h1>
                     <strong>Unterstützung, Information und Austausch für Familien</strong>
-                    <p>
+                    <p className='introductionHome'>
                         Das SHIA Familienzentrum ist ein Treffpunkt im Herzen Dessaus für die ganze Familie. <br />
                         Wir bieten ein breites Spektrum an Angeboten an, um euch in eurem Alltag zu unterstützen <br />
                         und euch die Möglichkeit zu geben, sich mit anderen Familien auszutauschen.
