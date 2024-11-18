8c61a8532c661da57cfd06d70e5d32e2b0b4eb13
sebastianbraeutigam <sebastian.lange4@icloud.com>
2024-11-18 23:47:00 +0100

    change events.json + eventTable.jsx

--- src/components/pages/home/eventTable.jsx
+++ src/components/pages/home/eventTable.jsx
@@ -5,28 +5,46 @@ import './EventTable.css';
 
 class EventTable extends Component {
     state = {  } 
+
+    // Funktion zum Sortieren der Events nach Wochentagen
+    sortEventsByWeekday(events) {
+        const weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
+        return events.flatMap(event => 
+            event.schedule.map(scheduleItem => ({
+                ...scheduleItem,
+                event: event.event,
+                page: event.page,
+                id: event.id
+            }))
+        ).sort((a, b) => weekdays.indexOf(a.weekday) - weekdays.indexOf(b.weekday));
+    }
+
     render() { 
-        return  <>
-                    <table className='eventTable'>
-                        <thead>
-                            <tr>
-                                <th>Event</th>
-                                <th>Wochentag</th>
-                                <th>Startzeit</th>
+        const sortedEvents = this.sortEventsByWeekday(events);
+
+        return (
+            <>
+                <table className='eventTable'>
+                    <thead>
+                        <tr>
+                            <th>Event</th>
+                            <th>Wochentag</th>
+                            <th>Startzeit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {sortedEvents.map((event, index) => (
+                            <tr key={`${event.id}-${index}`}>
+                                <td><NavLink to={event.page}>{event.event}</NavLink></td>
+                                <td>{event.weekday}</td>
+                                <td>{event.time}</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {events.map(event => (
-                                <tr key={event.id}>
-                                    <td><NavLink to={event.page}>{event.event}</NavLink></td>
-                                    <td>{event.weekday}</td>
-                                    <td>{event.time}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </>;
+                        ))}
+                    </tbody>
+                </table>
+            </>
+        );
     }
 }
- 
+
 export default EventTable;
