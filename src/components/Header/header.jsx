cb761727cad496ac76e5b913dfd799e84d44a57b
sebastianbraeutigam <sebastian.lange4@icloud.com>
2024-11-29 12:56:57 +0100

    changes header and hover-effects

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,7 +6,8 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isMenuOpen: false
+            isMenuOpen: false,
+            isHovered: false
         };
     }
 
@@ -18,8 +19,25 @@ class Header extends Component {
         this.setState({ isMenuOpen: false });
     }
 
+    handleMouseEnterAngebote = () => {
+        this.setState({ angeboteIsHovered: true });
+    }
+
+    handleMouseLeaveAngebote = () => {
+        this.setState({ angeboteIsHovered: false });
+    }
+
+    handleMouseEnterProjects = () => {
+        this.setState({ projectsIsHovered: true });
+    }
+
+    handleMouseLeaveProjects = () => {
+        this.setState({ projectsIsHovered: false });
+    }
+
+
     render() {
-        const { isMenuOpen } = this.state;
+        const { isMenuOpen, angeboteIsHovered, projectsIsHovered } = this.state;
 
         return (
             <header>
@@ -40,28 +58,25 @@ class Header extends Component {
                                 <li className="nav-item" style={{whiteSpace: 'nowrap'}}>
                                     <NavLink className="nav-link" to="/about" onClick={this.closeMenu}>Über uns</NavLink>
                                 </li>
-                                <li className="nav-item dropdown">
-                                    <button className="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <li className={`nav-item dropdown ${angeboteIsHovered ? 'show' : ''}`} onMouseEnter={this.handleMouseEnterAngebote} onMouseLeave={this.handleMouseLeaveAngebote}>
+                                    <NavLink className="nav-link dropdown-toggle" type="button" aria-expanded={angeboteIsHovered} to="/angebote-leistungen/ueberblick" onClick={this.closeMenu}>
                                         Angebote &amp; Leistungen
-                                    </button>
-                                    <ul className="dropdown-menu dropdown-menu-end">
-                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/ueberblick" onClick={this.closeMenu}>Überblick</NavLink></li>
-                                        <ul>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/achtsamkeitstraining" onClick={this.closeMenu}>Achtsamkeitstraining</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/informations-und-entlastungsgesraeche" onClick={this.closeMenu}>Informations- und Entlastungsgespräche</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/kinder-und-jugendlichensprechstunde" onClick={this.closeMenu}>Kinder- und Jugendlichensprechstunde</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/kinderyoga" onClick={this.closeMenu}>Kinderyoga</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/eltern-kind-gruppe" onClick={this.closeMenu}>Eltern-Kind-Gruppe</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/spielgruppe" onClick={this.closeMenu}>Spielgruppe</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="/angebote-leistungen/musikkreis" onClick={this.closeMenu}>Musikkreis</NavLink></li>
-                                        </ul>
+                                    </NavLink>
+                                    <ul className={`dropdown-menu dropdown-menu-end ${angeboteIsHovered ? 'show' : ''}`}>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/achtsamkeitstraining" onClick={this.closeMenu}>Achtsamkeitstraining</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/informations-und-entlastungsgesraeche" onClick={this.closeMenu}>Informations- und Entlastungsgespräche</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/kinder-und-jugendlichensprechstunde" onClick={this.closeMenu}>Kinder- und Jugendlichensprechstunde</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/kinderyoga" onClick={this.closeMenu}>Kinderyoga</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/eltern-kind-gruppe" onClick={this.closeMenu}>Eltern-Kind-Gruppe</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/spielgruppe" onClick={this.closeMenu}>Spielgruppe</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/angebote-leistungen/musikkreis" onClick={this.closeMenu}>Musikkreis</NavLink></li>
                                     </ul>
                                 </li>
-                                <li className="nav-item dropdown">
-                                    <button className="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <li className={`nav-item dropdown ${projectsIsHovered ? 'show' : ''}`} onMouseEnter={this.handleMouseEnterProjects} onMouseLeave={this.handleMouseLeaveProjects}>
+                                    <button className="nav-link dropdown-toggle" type="button" aria-expanded={projectsIsHovered}>
                                         Projekte
                                     </button>
-                                    <ul className="dropdown-menu dropdown-menu-end">
+                                    <ul className={`dropdown-menu dropdown-menu-end ${projectsIsHovered ? 'show' : ''}`}>
                                         <li><NavLink className="dropdown-item" to="/projekte/gelbe-feen" onClick={this.closeMenu}>gelbe Feen</NavLink></li>
                                         <li><NavLink className="dropdown-item" to="/projekte/oldi" onClick={this.closeMenu}>OLdi</NavLink></li>
                                     </ul>
