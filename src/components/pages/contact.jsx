031da52e675c77be9277e8cbdcdb494003fe1159
sebastianbraeutigam <sebastian.lange4@icloud.com>
2025-03-13 23:14:04 +0100

    change contact.jsx

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -37,7 +37,7 @@ class Contact extends Component {
                         <p className='email'>E-Mail: <a href="mailto:info@shia-dessau.de">info@shia-dessau.de</a></p> 
                     </div>
                     <div className="addressCard" ref={el => this.iframeContainer = el}>
-                        {this.state.loadIframe && (
+                        {this.state.loadIframe ? (
                             <iframe
                                 className='mapsIframe'
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2465.115327102633!2d12.248367777115122!3d51.84058948630762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a616d06b62adcd%3A0x5b20bf781d8b22a0!2sW%C3%B6rlitzer%20Str.%2069%2C%2006844%20Dessau-Ro%C3%9Flau!5e0!3m2!1sde!2sde!4v1731363374194!5m2!1sde!2sde"
@@ -46,6 +46,10 @@ class Contact extends Component {
                                 title="Google Maps"
                                 allowFullScreen
                             />
+                        ) : (
+                            <div className="iframe-placeholder">
+                                <p>Karte wird geladen...</p>
+                            </div>
                         )}
                     </div>
                 </div>
