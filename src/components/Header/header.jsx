9c48e54a2e38143c60d5e3a5c5c978b60fe36f75
sebastianbraeutigam <sebastian.lange4@icloud.com>
2025-03-23 23:23:01 +0100

    fix menubug

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,95 +1,99 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import './header.css';
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isMenuOpen: false,
-            angeboteIsHovered: false,
-            projectsIsHovered: false
-        };
-    }
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [angeboteIsHovered, setAngeboteIsHovered] = useState(false);
+    const [projectsIsHovered, setProjectsIsHovered] = useState(false);
+    const location = useLocation();
 
-    toggleMenu = () => {
-        this.setState({ isMenuOpen: !this.state.isMenuOpen });
-    }
+    useEffect(() => {
+        setIsMenuOpen(false);
+    }, [location]);
 
-    closeMenu = () => {
-        this.setState({ isMenuOpen: false });
-    }
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-    handleMouseEnter = (menu) => {
-        this.setState({ [menu]: true });
-    }
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
 
-    handleMouseLeave = (menu) => {
-        this.setState({ [menu]: false });
-    }
+    const handleMouseEnter = (menu) => {
+        if (menu === 'angebote') {
+            setAngeboteIsHovered(true);
+        } else if (menu === 'projects') {
+            setProjectsIsHovered(true);
+        }
+    };
 
-    renderDropdownMenu = (menu, items) => {
-        const isHovered = this.state[menu];
+    const handleMouseLeave = (menu) => {
+        if (menu === 'angebote') {
+            setAngeboteIsHovered(false);
+        } else if (menu === 'projects') {
+            setProjectsIsHovered(false);
+        }
+    };
+
+    const renderDropdownMenu = (menu, items) => {
+        const isHovered = menu === 'angebote' ? angeboteIsHovered : projectsIsHovered;
         return (
-            <li className={`nav-item dropdown ${isHovered ? 'show' : ''}`} onMouseEnter={() => this.handleMouseEnter(menu)} onMouseLeave={() => this.handleMouseLeave(menu)}>
+            <li className={`nav-item dropdown ${isHovered ? 'show' : ''}`} onMouseEnter={() => handleMouseEnter(menu)} onMouseLeave={() => handleMouseLeave(menu)}>
                 <button className="nav-link dropdown-toggle" type="button" aria-expanded={isHovered}>
-                    {menu === 'angeboteIsHovered' ? 'Angebote & Leistungen' : 'Projekte'}
+                    {menu === 'angebote' ? 'Angebote & Leistungen' : 'Projekte'}
                 </button>
                 <ul className={`dropdown-menu dropdown-menu-end ${isHovered ? 'show' : ''}`}>
                     {items.map((item, index) => (
-                        <li key={index}><NavLink className="dropdown-item" to={item.path} onClick={this.closeMenu}>{item.label}</NavLink></li>
+                        <li key={index}><NavLink className="dropdown-item" to={item.path} onClick={closeMenu}>{item.label}</NavLink></li>
                     ))}
                 </ul>
             </li>
         );
-    }
+    };
 
-    render() {
-        const { isMenuOpen } = this.state;
+    const angeboteItems = [
+        { path: '/angebote-leistungen/achtsamkeitstraining', label: 'Achtsamkeitstraining' },
+        { path: '/angebote-leistungen/informations-und-entlastungsgesraeche', label: 'Informations- und Entlastungsgespräche' },
+        { path: '/angebote-leistungen/kinder-und-jugendlichensprechstunde', label: 'Kinder- und Jugendlichensprechstunde' },
+        { path: '/angebote-leistungen/eltern-kind-gruppe', label: 'Eltern-Kind-Gruppe' },
+        { path: '/angebote-leistungen/spielgruppe', label: 'Spielgruppe' },
+        { path: '/angebote-leistungen/musikkreis', label: 'Musikkreis' }
+    ];
 
-        const angeboteItems = [
-            { path: '/angebote-leistungen/achtsamkeitstraining', label: 'Achtsamkeitstraining' },
-            { path: '/angebote-leistungen/informations-und-entlastungsgesraeche', label: 'Informations- und Entlastungsgespräche' },
-            { path: '/angebote-leistungen/kinder-und-jugendlichensprechstunde', label: 'Kinder- und Jugendlichensprechstunde' },
-            { path: '/angebote-leistungen/eltern-kind-gruppe', label: 'Eltern-Kind-Gruppe' },
-            { path: '/angebote-leistungen/spielgruppe', label: 'Spielgruppe' },
-            { path: '/angebote-leistungen/musikkreis', label: 'Musikkreis' }
-        ];
+    const projectsItems = [
+        { path: '/projekte/gelbe-feen', label: 'gelbe Feen' },
+        { path: '/projekte/oldi', label: 'OLdi' },
+        { path: '/projekte/klein-hilft-gross', label: 'Klein hilft Groß' }
+    ];
 
-        const projectsItems = [
-            { path: '/projekte/gelbe-feen', label: 'gelbe Feen' },
-            { path: '/projekte/oldi', label: 'OLdi' },
-            { path: '/projekte/klein-hilft-gross', label: 'Klein hilft Groß' }
-        ];
+    return (
+        <header>
+            <nav className="navbar navbar-expand-md bg-*">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand imgTag" to="/" onClick={closeMenu}>
+                        <img src={`${process.env.PUBLIC_URL}/assets/img/logo_shia.png`} alt="Shia Logo" />
+                    </NavLink>
+                    <button className={`navbar-toggler ${isMenuOpen ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isMenuOpen ? 'true' : 'false'} aria-label="Toggle navigation" onClick={toggleMenu}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-        return (
-            <header>
-                <nav className="navbar navbar-expand-md bg-*">
-                    <div className="container-fluid">
-                        <NavLink className="navbar-brand imgTag" to="/" onClick={this.closeMenu}>
-                            <img src={`${process.env.PUBLIC_URL}/assets/img/logo_shia.png`} alt="Shia Logo" />
-                        </NavLink>
-                        <button className={`navbar-toggler ${isMenuOpen ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isMenuOpen ? 'true' : 'false'} aria-label="Toggle navigation" onClick={this.toggleMenu}>
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
-                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <NavLink className="nav-link navlink" to="/" onClick={this.closeMenu}>Home</NavLink>
-                                </li>
-                                <li className="nav-item" style={{whiteSpace: 'nowrap'}}>
-                                    <NavLink className="nav-link navlink" to="/about" onClick={this.closeMenu}>Über uns</NavLink>
-                                </li>
-                                {this.renderDropdownMenu('angeboteIsHovered', angeboteItems)}
-                                {this.renderDropdownMenu('projectsIsHovered', projectsItems)}
-                            </ul>
-                        </div>
+                    <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link navlink" to="/" onClick={closeMenu}>Home</NavLink>
+                            </li>
+                            <li className="nav-item" style={{whiteSpace: 'nowrap'}}>
+                                <NavLink className="nav-link navlink" to="/about" onClick={closeMenu}>Über uns</NavLink>
+                            </li>
+                            {renderDropdownMenu('angebote', angeboteItems)}
+                            {renderDropdownMenu('projects', projectsItems)}
+                        </ul>
                     </div>
-                </nav>
-            </header>
-        );
-    }
-}
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
