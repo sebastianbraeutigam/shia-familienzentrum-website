3dfef07089a899b76ae89193b9478ad8b7b29493
sebastianbraeutigam <sebastian.lange4@icloud.com>
2025-04-29 04:27:37 +0200

    add scrolltotop.jsx

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App';
+import ScrollToTop from './components/scrolltotop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
         <BrowserRouter basename="/shia-familienzentrum-website">
+            <ScrollToTop />
             <App />
         </BrowserRouter>
     </React.StrictMode>
