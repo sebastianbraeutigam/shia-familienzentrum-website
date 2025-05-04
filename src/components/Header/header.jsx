eeffea10c764e9c355336df09e24138c35310b83
sebastianbraeutigam <sebastian.lange4@icloud.com>
2025-05-04 13:12:40 +0200

    Change Navbar and Styling

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,16 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [angeboteIsHovered, setAngeboteIsHovered] = useState(false);
-    const [projectsIsHovered, setProjectsIsHovered] = useState(false);
-    const location = useLocation();
-
-    useEffect(() => {
-        setIsMenuOpen(false);
-    }, [location]);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -18,38 +11,6 @@ const Header = () => {
 
     const closeMenu = () => {
         setIsMenuOpen(false);
-    };
-
-    const handleMouseEnter = (menu) => {
-        if (menu === 'angebote') {
-            setAngeboteIsHovered(true);
-        } else if (menu === 'projects') {
-            setProjectsIsHovered(true);
-        }
-    };
-
-    const handleMouseLeave = (menu) => {
-        if (menu === 'angebote') {
-            setAngeboteIsHovered(false);
-        } else if (menu === 'projects') {
-            setProjectsIsHovered(false);
-        }
-    };
-
-    const renderDropdownMenu = (menu, items) => {
-        const isHovered = menu === 'angebote' ? angeboteIsHovered : projectsIsHovered;
-        return (
-            <li className={`nav-item dropdown ${isHovered ? 'show' : ''}`} onMouseEnter={() => handleMouseEnter(menu)} onMouseLeave={() => handleMouseLeave(menu)}>
-                <button className="nav-link dropdown-toggle" type="button" aria-expanded={isHovered}>
-                    {menu === 'angebote' ? 'Angebote & Leistungen' : 'Projekte'}
-                </button>
-                <ul className={`dropdown-menu dropdown-menu-end ${isHovered ? 'show' : ''}`} onClick={closeMenu}>
-                    {items.map((item, index) => (
-                        <li key={index}><NavLink className="dropdown-item" to={item.path} onClick={closeMenu}>{item.label}</NavLink></li>
-                    ))}
-                </ul>
-            </li>
-        );
     };
 
     const angeboteItems = [
@@ -69,27 +30,46 @@ const Header = () => {
 
     return (
         <header>
-            <nav className="navbar navbar-expand-md bg-*">
-                <div className="container-fluid">
-                    <NavLink className="navbar-brand imgTag" to="/" onClick={closeMenu}>
+            <nav className="navbar">
+                <div className="container">
+                    <NavLink className="navbar-brand" to="/" onClick={closeMenu}>
                         <img src={`${process.env.PUBLIC_URL}/assets/img/logoshia.png`} alt="Shia Logo" />
                     </NavLink>
-                    <button className={`navbar-toggler ${isMenuOpen ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isMenuOpen ? 'true' : 'false'} aria-label="Toggle navigation" onClick={toggleMenu}>
-                        <span className="navbar-toggler-icon"></span>
+                    <button className="hamburger" onClick={toggleMenu}>
+                        ☰
                     </button>
-
-                    <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <NavLink className="nav-link navlink" to="/" onClick={closeMenu}>Home</NavLink>
-                            </li>
-                            <li className="nav-item" style={{whiteSpace: 'nowrap'}}>
-                                <NavLink className="nav-link navlink" to="/about" onClick={closeMenu}>Über uns</NavLink>
-                            </li>
-                            {renderDropdownMenu('angebote', angeboteItems)}
-                            {renderDropdownMenu('projects', projectsItems)}
-                        </ul>
-                    </div>
+                    <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+                        <li>
+                            <NavLink className="nav-link" to="/" onClick={closeMenu}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="nav-link" to="/about" onClick={closeMenu}>Über uns</NavLink>
+                        </li>
+                        <li className="dropdown">
+                            <button className="dropdown-toggle">Angebote & Leistungen</button>
+                            <ul className="dropdown-menu">
+                                {angeboteItems.map((item, index) => (
+                                    <li key={index}>
+                                        <NavLink className="dropdown-item" to={item.path} onClick={closeMenu}>
+                                            {item.label}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                        <li className="dropdown">
+                            <button className="dropdown-toggle">Projekte</button>
+                            <ul className="dropdown-menu">
+                                {projectsItems.map((item, index) => (
+                                    <li key={index}>
+                                        <NavLink className="dropdown-item" to={item.path} onClick={closeMenu}>
+                                            {item.label}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
