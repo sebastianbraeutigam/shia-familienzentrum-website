c3403649f38021d5219e1a8b883af26cf27fe823
sebastianbraeutigam <sebastian.lange4@icloud.com>
2025-05-11 22:45:47 +0200

    edit icon-size

--- src/components/Header/CloseIcon.jsx
+++ src/components/Header/CloseIcon.jsx
@@ -1,6 +1,6 @@
 const CloseIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" className="bi bi-x" viewBox="0 0 24 24">
-        <path stroke="#EF9C66" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+        <path stroke="#EF9C66" strokeWidth="2" strokeLinecap="round" d="M4 4L20 20M20 4L4 20" />
     </svg>
 );
 
