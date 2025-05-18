801862a5d2fa3084cefd551a5075563c2e83a009
sebastianbraeutigam <sebastian.lange4@icloud.com>
2025-05-18 12:58:55 +0200

    change events-list

--- src/components/Header/CloseIcon.jsx
+++
@@ -1,7 +0,0 @@
-const CloseIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" className="bi bi-x" viewBox="0 0 24 24">
-        <path stroke="#EF9C66" strokeWidth="2" strokeLinecap="round" d="M4 4L20 20M20 4L4 20" />
-    </svg>
-);
-
-export default CloseIcon;
